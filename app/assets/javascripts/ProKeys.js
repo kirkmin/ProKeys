28f6f48236529cd72f9dbf2be63550c2076cd1f4
Kirk Min <kimchikiller1990@gmail.com>
2016-12-20 03:24:50 -0800

    moved all modal script to Prokeys. Refractored to jquery so the dynamically rendered modals wont repeat

--- app/assets/javascripts/ProKeys.js
+++ app/assets/javascripts/ProKeys.js
@@ -12,9 +12,39 @@ window.ProKeys = {
 };
 
 $(document).ready(function(){
+  // Parallax
   $("body").mousemove(function(e){
     var amountMovedX = (e.pageX * -1 / 30);
     var amountMovedY = (e.pageY * -1 / 30);
     $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
   });
+
+  // Modal logic
+  $("#nav_grid").on("click", "#logInLink", function() {
+    console.log("LOL")
+    $("#logInModal").css("display", "block")
+    $("#toggle")[0].checked = false
+  })
+
+  $("#nav_grid").on("click", "#signUpLink", function() {
+      $("#signUpModal").css("display", "block")
+    $("#toggle")[0].checked = false
+  })
+
+  $("#nav_grid").on("click", ".close", function () {
+    $("#logInModal").css("display", "none")
+    $("#signUpModal").css("display", "none")
+  })
+
+  $(window).on("click", function(event) {
+      if (event.target == $("#logInModal")[0]) {
+        $("#logInModal").css("display", "none")
+      } else if (event.target == $("#signUpModal")[0]) {
+        $("#signUpModal").css("display", "none")
+      }
+  })
+
+  $("nav #right li").on("click", function () {
+    $("#toggle")[0].checked = false;
+  })
 });
