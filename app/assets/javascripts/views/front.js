9eab88e9855492c1588ed06874e3fe8edbf8c860
Kirk Min <kimchikiller1990@gmail.com>
2017-02-28 04:47:16 -0800

    had to add erb function to some util javascript files to get the correct asset pipeline for the correct digest. Also had to create some objects containing the correct path...pretty janky

--- app/assets/javascripts/views/front.js
+++ app/assets/javascripts/views/front.js
@@ -5,11 +5,11 @@ ProKeys.Views.Front = Backbone.CompositeView.extend({
 		this.makeAudio();
 	    $(document).on('keydown', _.bind(this.soundOn, this));
 	    $(document).on('keyup', _.bind(this.soundOff, this));
-	    $(this).on("DOMException", function (e, code) {
-	    	this.audios[code] = this.audios[code] || new Audio('../assets/notes/' + this.defaultKeys[code] + '.mp3')
-	    	this.audios[code].pause()
-	    	$(this.audios[code]).data("playing", false)
-	    })
+	    // $(this).on("DOMException", function (e, code) {
+	    // 	this.audios[code] = this.audios[code] || new Audio('../assets/notes/' + this.defaultKeys[code] + '.mp3')
+	    // 	this.audios[code].pause()
+	    // 	$(this.audios[code]).data("playing", false)
+	    // })
 	},
 
 	render: function () {
@@ -23,7 +23,7 @@ ProKeys.Views.Front = Backbone.CompositeView.extend({
 		var that = this;
 		this.audios = {}
 		_.each(that.defaultKeys, function (value, key) {
-			that.audios[key] = new Audio('../assets/notes/' + value + '.mp3')
+			that.audios[key] = new Audio(that.soundPath[value])
 			$(that.audios[key]).data("playing", false)
 		})
 	},
@@ -33,9 +33,10 @@ ProKeys.Views.Front = Backbone.CompositeView.extend({
 		if (audio && !$(audio).data("playing")) {
 			$(audio).data("playing", true)
 			audio.play().catch(function (e) {
+				debugger
 				console.log("Caught Chrome error DOMException")
-				this.audios[e.keyCode] = new Audio('../assets/notes/' + this.defaultKeys[e.keyCode] + '.mp3')
-				$(this).trigger("DOMException", [e.keyCode]) // insure that there is audio & is ready to play on next press...
+				this.audios[e.keyCode] = new Audio(this.soundPath[this.defaultKeys[e.keyCode]])
+				// $(this).trigger("DOMException", [e.keyCode]) // insure that there is audio & is ready to play on next press...
 			}.bind(this, event));
 		}
 	},
@@ -48,7 +49,6 @@ ProKeys.Views.Front = Backbone.CompositeView.extend({
 			$(audio).data("playing", false)
 		}
 		// else if (audio && !$(audio).data("playing")) {
-		// 	debugger
 		// 	$(audio).data("playing", false)
 		// }
 	},
