1cd6cf26c7a916c7187bf0a2b1260873fd9bce1a
Kirk Min <kimchikiller1990@gmail.com>
2017-04-29 02:00:30 -0700

    fixed bug where the body would not scroll be adding some code to the javascript views for backend.

--- app/views/sessions/destroy.js
+++ app/views/sessions/destroy.js
@@ -1,4 +1,5 @@
 ProKeys.currentUserId = null
 $(document).trigger('log-out');
 $("#nav_grid").html("<%= escape_javascript(render partial: 'layouts/navbar') %>");
+$("body").css("overflow", "scroll")
 Backbone.history.navigate("", {trigger: true})
